2fbbfbdcc61d088a01c6c431feeadc46782bbcb5
Varun Shahdadpuri <shahdadpuri.varun@gmail.com>
2018-11-30 01:41:43 -0800

    Code cleanup

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,5 @@
 var env = process.env.NODE_ENV || 'development';
 
-
 if (env === 'development' || env === 'test') {
    var config = require('./config.json');
    var envConfig = config[env];
@@ -8,11 +7,3 @@ if (env === 'development' || env === 'test') {
       process.env[key] = envConfig[key];
    });
 }
-
-// if ( env === 'development') {
-//     process.env.PORT = 3000;
-//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-//  } else if (env === 'test') {
-//     process.env.PORT = 3000;
-//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-//  }
